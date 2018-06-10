aa8ff782d26027cf15645993327a4865827ec17e
Berk Orbay <berk.orbay@algopoly.com>
2018-06-10 20:23:35 +0300

    init

---
+++ R/evds_functions.R
@@ -0,0 +1,1 @@
+httr::GET("https://evds2.tcmb.gov.tr/service/evds/series=TP.DK.USD.A-TP.DK.EUR.A-TP.DK.CHF.A-TP.DK.GBP.A-TP.DK.JPY.A&startDate=01-10-2017&endDate=01-11-2017&type=xml&key=0FDp4bYN0I")
