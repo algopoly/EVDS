67d1918aac242a700477afc3571e27c19777eee7
Berk Orbay <berk.orbay@algopoly.com>
2018-06-10 20:52:40 +0300

    first push

--- R/evds_functions.R
+++ R/evds_functions.R
@@ -1,1 +1,43 @@
-httr::GET("https://evds2.tcmb.gov.tr/service/evds/series=TP.DK.USD.A-TP.DK.EUR.A-TP.DK.CHF.A-TP.DK.GBP.A-TP.DK.JPY.A&startDate=01-10-2017&endDate=01-11-2017&type=xml&key=0FDp4bYN0I")
+#'@export
+#'@title Get Series
+get_series <- function(series=c("TP.DK.USD.A","TP.DK.EUR.A"),start_date="01-10-2017",end_date="01-11-2017"){
+
+  api_key = Sys.getenv("EVDS_KEY")
+
+  series = paste(series,collapse="-")
+
+  the_response <-
+  httr::GET(paste0("https://evds2.tcmb.gov.tr/service/evds/series=",series,"&startDate=",start_date,"&endDate=",end_date,"&type=json&key=",api_key))
+
+  json_content<-
+  httr::content(the_response,as="text")
+
+  jsonlite::fromJSON(json_content)
+}
+
+#'@export
+#'@title Set API KEY
+set_evds_key <- function(api_key){
+
+  Sys.setenv(EVDS_KEY = api_key)
+
+}
+
+#'@export
+#'@title Data Categories
+get_category_list<-function(){
+
+  api_key = Sys.getenv("EVDS_KEY")
+
+  series = paste(series,collapse="-")
+
+  the_response <-
+  httr::GET(paste0("https://evds2.tcmb.gov.tr/service/evds/categories/type=json&key=",api_key))
+
+  json_content<-
+  httr::content(the_response,as="text")
+
+  jsonlite::fromJSON(json_content)
+
+
+}
