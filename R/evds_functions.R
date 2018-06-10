c3dc45a20f13be066506ced44a1e12baedaf7e5b
Berk Orbay <berk.orbay@algopoly.com>
2018-06-10 21:54:49 +0300

    version 0.1.0
    Fundamental functions only.

--- R/evds_functions.R
+++ R/evds_functions.R
@@ -1,21 +1,4 @@
 #'@export
-#'@title Get Series
-get_series <- function(series=c("TP.DK.USD.A","TP.DK.EUR.A"),start_date="01-10-2017",end_date="01-11-2017"){
-
-  api_key = Sys.getenv("EVDS_KEY")
-
-  series = paste(series,collapse="-")
-
-  the_response <-
-  httr::GET(paste0("https://evds2.tcmb.gov.tr/service/evds/series=",series,"&startDate=",start_date,"&endDate=",end_date,"&type=json&key=",api_key))
-
-  json_content<-
-  httr::content(the_response,as="text")
-
-  jsonlite::fromJSON(json_content)
-}
-
-#'@export
 #'@title Set API KEY
 set_evds_key <- function(api_key){
 
@@ -23,21 +6,83 @@ set_evds_key <- function(api_key){
 
 }
 
-#'@export
-#'@title Data Categories
-get_category_list<-function(){
+#'@title Common Wrapper for Data Parsing
+get_data_from_phrase <- function(the_phrase){
 
   api_key = Sys.getenv("EVDS_KEY")
 
-  series = paste(series,collapse="-")
+  if(api_key == ""){
+    stop("You should set your API key. See ?set_evds_key function.")
+  }
+
+  the_phrase <- paste0(the_phrase,"&type=json&key=",api_key)
 
   the_response <-
-  httr::GET(paste0("https://evds2.tcmb.gov.tr/service/evds/categories/type=json&key=",api_key))
+  httr::GET(the_phrase)
+
+  if(the_response$status == 500){
+    stop("Status 500. Check API Key.")
+  }
 
   json_content<-
-  httr::content(the_response,as="text")
+  httr::content(the_response,as="text",encoding="UTF-8")
 
   jsonlite::fromJSON(json_content)
+
+}
+
+#'@export
+#'@title Get Series
+get_series <- function(series=c("TP.DK.USD.A","TP.DK.EUR.A"),start_date="01-10-2017",end_date="01-11-2017"){
+
+  series = paste(series,collapse="-")
+
+  the_phrase <- paste0("https://evds2.tcmb.gov.tr/service/evds/series=",series,"&startDate=",start_date,"&endDate=",end_date)
+
+  get_data_from_phrase(the_phrase)
+
+}
+
+
+#'@export
+#'@title Data Groups
+show_datagroups<-function(data_mode="all"){
+
+  if(data_mode=="all"){
+    data_mode <- 0
+  }else{
+    stop("This part is not yet finished.")
+  }
+
+  the_phrase<-
+  paste0("https://evds2.tcmb.gov.tr/service/evds/datagroups/mode=",data_mode)
+
+  get_data_from_phrase(the_phrase)
+
+
+}
+
+
+#'@export
+#'@title Data Categories
+show_categories<-function(){
+
+  the_phrase<-
+  paste0("https://evds2.tcmb.gov.tr/service/evds/categories/")
+
+  get_data_from_phrase(the_phrase)
+
+
+}
+
+#'@export
+#'@title List of series based on a category code or name
+show_serie_lists<-function(ccode){
+
+  the_phrase<-
+  paste0("https://evds2.tcmb.gov.tr/service/evds/serieList/code=",ccode)
+
+  get_data_from_phrase(the_phrase)
 
 
 }
