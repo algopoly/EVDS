5fed875ad1a5b9edc224ba39ab38279df0657299
utkuhangenc <40195837+utkuhangenc@users.noreply.github.com>
2018-07-04 11:52:52 +0300

    function documentation

--- R/evds_functions.R
+++ R/evds_functions.R
@@ -41,14 +41,14 @@ get_data_from_phrase <- function(the_phrase){
 #' Get series Function
 #'
 #' This function allows you to get series you want to work on from the EVDS website. You can also choose the dates you want to get the data from.
-#' @param series,start_date,end_date The desired series should be put in the series in a vector form. start_date and end_date are in form of `date_format`.
+#' @param series,start_date,end_date The desired series should be put in the series in a vector form.
+#' @param start_date,end_date These are in form of `date_format`.
 #' @param date_format Date format. If Ymd, it is transformed to dd-mm-YYYY format because of CBRT EVDS requirements. It only accepts Ymd (YYYY-mm-dd) or dmY as input. (Default format: dmY)
 #' @export
 #' @examples
-#' get_series(series=c("",""), start_date = "dd-mm-YYYY", end_date = "dd-mm-YYYY")
+#' get_series(series=c("TP.DK.USD.A","TP.APIFON4"), start_date = "20-05-2010", end_date = "20-05-2018")
 #' @title Get Series
 get_series <- function(series=c("TP.DK.USD.A","TP.DK.EUR.A"),start_date="01-10-2017",end_date="01-11-2017",date_format="dmY"){
-<<<<<<< HEAD
 
   if(date_format=="Ymd"){
     start_date <- format(as.Date(start_date),"%d-%m-%Y")
@@ -56,16 +56,7 @@ get_series <- function(series=c("TP.DK.USD.A","TP.DK.EUR.A"),start_date="01-10-2
   }else if(date_format != "dmY"){
     stop("Wrong date format!")
   }
-=======
->>>>>>> dc4f67fa9a87637a0cf78606a287a8d6dbfa77a5
 
-  if(date_format=="Ymd"){
-    start_date <- format(as.Date(start_date),"%d-%m-%Y")  
-    end_date <- format(as.Date(end_date),"%d-%m-%Y")  
-  }else if(date_format != "dmY"){
-    stop("Wrong date format!")
-  }
-  
   series = paste(series,collapse="-")
 
   the_phrase <- paste0("https://evds2.tcmb.gov.tr/service/evds/series=",series,"&startDate=",start_date,"&endDate=",end_date)
@@ -96,7 +87,7 @@ show_datagroups<-function(data_mode="all"){
 
 #' Show Data Categories Function
 #'
-#' This function allows you to preview the category of series you want to work on from the EVDS website.
+#' This function allows you to preview the category of series you want to work on from the EVDS website. You need this function to preview category names.This category name could further used in previewing the data groups.
 #' @export
 #' @title Data Categories
 show_categories<-function(){
@@ -110,11 +101,12 @@ show_categories<-function(){
 }
 
 
-#' Show Data Groups Function
+#' Show Series Based on Category Function
 #'
-#' This function allows you to preview the series you want to work on from the EVDS website.
+#' This function allows you to preview the series from a specific data group you want to work on from the EVDS website.You can get the name of the datagroups by using `show_categories`function.
 #' @param ccode Category code
 #' @export
+#' @example show_serie_lists("bie_fafaiz")
 #' @title List of series based on a category code or name
 show_serie_lists<-function(ccode){
 
@@ -122,6 +114,27 @@ show_serie_lists<-function(ccode){
   paste0("https://evds2.tcmb.gov.tr/service/evds/serieList/code=",ccode)
 
   get_data_from_phrase(the_phrase)
+
+
+}
+
+#' Show All Series Function
+#'
+#' This function allows you to preview all the series on the EVDS website.
+#' @export
+#' @title List of series
+all_series <- function(){
+
+
+  all_dg <- show_datagroups()
+
+  the_df<- show_serie_lists(all_dg$DATAGROUP_CODE[1])
+
+  for(i in 2:length(all_dg$DATAGROUP_CODE)){
+    the_df <- bind_rows(the_df,show_serie_lists(all_dg$DATAGROUP_CODE[i]))
+  }
+
+  write_xlsx(the_df, "allSeries.xlsx")
 
 
 }
